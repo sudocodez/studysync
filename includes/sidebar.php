1435e31a4021422a4f51577b0177b66508e7a335
dannie <dannie@studysync.app>
2026-06-20 01:32:18 +0300

    Replace book SVG icon with giraffe/kiut logo (logo1.png), crop bottom 30% to remove 'exploring heights' text

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -13,12 +13,7 @@ if (isset($pdo, $_SESSION['user_id'])) {
     <div class="sidebar-header">
         <div class="logo">
             <div class="logo-icon">
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
-                    <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
-                    <circle cx="16" cy="8" r="3" stroke-width="1.2"/>
-                    <path d="M14.5 9.5 16 8l1.5 1.5"/>
-                </svg>
+                <img src="logo1.png" alt="Kiut" class="logo-img">
             </div>
             <span class="logo-text">StudySync</span>
         </div>
